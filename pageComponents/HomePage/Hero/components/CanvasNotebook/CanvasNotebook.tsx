8f6cbb6a612e4e0fae1e3b119bfa28c4ed392428
Sergey Maksimenko <serega.maksimenko@gmail.com>
2021-11-02 17:20:09 +0600

    test continue

--- pageComponents/HomePage/Hero/components/CanvasNotebook/CanvasNotebook.tsx
+++ pageComponents/HomePage/Hero/components/CanvasNotebook/CanvasNotebook.tsx
@@ -18,7 +18,6 @@ const CanvasNotebook = ({ scrollHeight, numFrames, width, height }: any) => {
 
   function preloadImages() {
     for (let i = 0; i <= numFrames; i++) {
-      console.log(getCurrentFrame(i));
       const img = new Image();
       const imgSrc = getCurrentFrame(i);
       img.src = imgSrc;
@@ -27,7 +26,9 @@ const CanvasNotebook = ({ scrollHeight, numFrames, width, height }: any) => {
   }
 
   const handleScroll = () => {
-    const scrollFraction = window.scrollY / (scrollHeight - window.innerHeight);
+    const scrollFraction = window.scrollY / (2500 - window.innerHeight);
+    console.log('scrollFraction', scrollFraction);
+
     const index = Math.min(
       numFrames - 1,
       Math.ceil(scrollFraction * numFrames)
@@ -36,87 +37,71 @@ const CanvasNotebook = ({ scrollHeight, numFrames, width, height }: any) => {
     if (index <= 0 || index > numFrames) {
       return;
     }
+    console.log('index', index);
 
     setFrameIndex(index);
   };
 
+  const test = {
+    frame: frameIndex,
+  };
   const renderCanvas = () => {
     const context = canvasRef!.current!.getContext('2d');
     context!.canvas.width = width;
     context!.canvas.height = height;
   };
-  // const sxcrollTriggerRef = useRef(
 
-  //   gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: canvasWrapRef.current,
-  //       start: 'top top',
-  //       // end: '+=' ,
-  //       pin: true,
-  //       markers: true,
-  //       scrub: true,
-  //       // onUpdate: handleScroll,
-  //     },
-  //   })
-  // );
-  // const sxcrollTriggerRef = useRef(gsap.to(canvasRef.current, {
-  //   scrollTrigger: {
-  //     trigger: canvasWrapRef.current,
-  //     start: 'top top',
-  //     // end: '+=' ,
-  //     pin: true,
-  //     markers: true,
-  //     scrub: true,
-  //     // onUpdate: handleScroll,
-  //   },
-  // }))
+  // console.log('asd', frameIndex);
+
   useEffect(() => {
     preloadImages();
     renderCanvas();
-    // if (!canvasRef.current || images.length < 1) {
-    //   return;
-    // }
-    const context = canvasRef!.current!.getContext('2d');
-    let requestId: any;
 
-    const render = () => {
-      context!.drawImage(
-        images[frameIndex],
-        0,
-        0,
-        1100,
-        (1100 * images[frameIndex].height) / images[frameIndex].width
-      );
-      requestId = requestAnimationFrame(render);
-    };
-    gsap.to(canvasRef.current, {
+    gsap.to(test, {
+      frame: numFrames - 1,
+      snap: 'frame',
       scrollTrigger: {
         trigger: canvasWrapRef.current,
         start: 'top top',
-        // end: '+=' ,
         pin: true,
         markers: true,
-        scrub: true,
-        // refreshPriority: 1,
-        onUpdate: () => {
-          console.log('update');
-          // // handleScroll();
-          // const scrollFraction =
-          //   window.scrollY / (scrollHeight - window.innerHeight);
-          // const index = Math.min(
-          //   numFrames - 1,
-          //   Math.ceil(scrollFraction * numFrames)
-          // );
-
-          // if (index <= 0 || index > numFrames) {
-          //   return;
-          // }
-
-          // setFrameIndex(index);
-          // render();
-        },
+        scrub: 1,
+      },
+      onUpdate: () => {
+        console.log('test', test);
+        const context = canvasRef!.current!.getContext('2d');
+        context?.drawImage(
+          images[test.frame],
+          0,
+          0,
+          1100,
+          (1100 * images[test.frame].height) / images[test.frame].width
+        );
+        // render(test.frame);
       },
     });
+
+    // const t = gsap.to(canvasRef.current, {
+    //   scrollTrigger: {
+    //     trigger: canvasWrapRef.current,
+    //     start: 'top top',
+    //     end: '+=2500',
+    //     pin: true,
+    //     markers: true,
+    //     scrub: true,
+    //     onUpdate: () => {
+    //       // if (frameIndex <= 0 || frameIndex > numFrames) {
+    //       //   return;
+    //       // }
+    //       // const index = frameIndex + 1;
+    //       // handleScroll();
+
+    //       // setFrameIndex((prevIndex) => prevIndex + 1);
+    //       // console.log('update', frameIndex);
+    //       // handleScroll();
+    //     },
+    //   },
+    // });
     // window.addEventListener('scroll', handleScroll);
     // return () => {
     //   // t.kill();
@@ -124,33 +109,30 @@ const CanvasNotebook = ({ scrollHeight, numFrames, width, height }: any) => {
     //   // window.removeEventListener('scroll', handleScroll);
     //   // cancelAnimationFrame(requestId);
     // };
-    return () => window.removeEventListener('scroll', handleScroll);
+
+    // return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  useEffect(() => {
-    if (!canvasRef.current || images.length < 1) {
-      return;
-    }
-    console.log('asd');
-
-    const context = canvasRef.current.getContext('2d');
-    let requestId: any;
-
-    const render = () => {
-      context!.drawImage(
-        images[frameIndex],
-        0,
-        0,
-        1100,
-        (1100 * images[frameIndex].height) / images[frameIndex].width
-      );
-      requestId = requestAnimationFrame(render);
-    };
-
-    render();
-
-    return () => cancelAnimationFrame(requestId);
-  }, [frameIndex, images]);
+  // useEffect(() => {
+  // if (!canvasRef.current || images.length < 1) {
+  //   return;
+  // }
+  // console.log('asd', frameIndex);
+  // const context = canvasRef.current.getContext('2d');
+  // let requestId: any;
+  // const render = () => {
+  //   context!.drawImage(
+  //     images[frameIndex],
+  //     0,
+  //     0,
+  //     1100,
+  //     (1100 * images[frameIndex].height) / images[frameIndex].width
+  //   );
+  //   requestId = requestAnimationFrame(render);
+  // };
+  // render();
+  // return () => cancelAnimationFrame(requestId);
+  // }, [frameIndex, images]);
   return (
     <div ref={canvasWrapRef} style={{ height: scrollHeight }}>
       <canvas ref={canvasRef} />
