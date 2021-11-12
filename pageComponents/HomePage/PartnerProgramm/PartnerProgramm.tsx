719c54cc06210708a888a603edff4f0f3d74955c
Sergey Maksimenko <serega.maksimenko@gmail.com>
2021-11-12 10:39:43 +0600

    start

---
+++ pageComponents/HomePage/PartnerProgramm/PartnerProgramm.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+const PartnerProgramm: FC = () => {
+  return <Root></Root>;
+};
+
+const Root = styled.section``;
+
+export default PartnerProgramm;
