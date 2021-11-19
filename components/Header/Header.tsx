7c16a2512b6f0753c01dd3a40fa3665a0f302f7b
Sergey Maksimenko <serega.maksimenko@gmail.com>
2021-11-19 17:56:02 +0600

    fixes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useRef, useState } from 'react';
 import { MobileHeader } from './components/MobileHeader';
 import { DesktopHeader } from './components/DesktopHeader';
+import { throttle } from 'throttle-debounce';
 
 interface HeaderProps {
   userAgent: { [key: string]: any };
@@ -8,18 +9,17 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ children, userAgent }) => {
   const [isMobileHeader, setIsMobileHeader] = useState(true);
-  const [windowWidth, setWindowWidth] = useState(0);
 
-  const handleResize = () => setWindowWidth(innerWidth);
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  useEffect(() => {
+  const handleResize = () => {
     innerWidth > 900 ? setIsMobileHeader(false) : setIsMobileHeader(true);
-  }, [windowWidth]);
+  };
+
+  const throttleHandleResize = throttle(500, handleResize);
+
+  useEffect(() => {
+    window.addEventListener('resize', throttleHandleResize);
+    return () => window.removeEventListener('resize', throttleHandleResize);
+  }, []);
 
   return (
     <>
