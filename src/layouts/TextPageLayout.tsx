2c59b75a8aa3cce5c25b1df58fde9ab704a840a0
Sergey Maksimenko <serega.maksimenko@gmail.com>
2021-11-30 17:22:06 +0600

    add context to inner pages

--- src/layouts/TextPageLayout.tsx
+++ src/layouts/TextPageLayout.tsx
@@ -1,21 +1,30 @@
-import { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled from 'styled-components';
 import { Footer } from '@/components/Footer';
 
 import { Cookies } from '@/components/Cookies';
 import { Header } from '@/components/Header';
 import HeaderBottom from '@/pageComponents/TextPage/HeaderBottom/HeaderBottom';
+import { MobileMenuContext } from '@/pages';
 
 interface UserAgent {
   userAgent: { [key: string]: any };
 }
 
 const TextPageLayout: FC<UserAgent> = ({ children, userAgent }) => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const contextMobileMenuValue = {
+    isOpenMenu,
+    setIsOpenMenu,
+    userAgent,
+  };
   return (
     <Root>
-      <Header userAgent={userAgent}>
-        <HeaderBottom />
-      </Header>
+      <MobileMenuContext.Provider value={contextMobileMenuValue}>
+        <Header>
+          <HeaderBottom />
+        </Header>
+      </MobileMenuContext.Provider>
       {children}
       <Footer />
       <Cookies />
