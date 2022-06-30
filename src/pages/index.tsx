9543cd839fa7ed5c436db8ea048c26855c6ec145
Sergey Maksimenko <serega.maksimenko@gmail.com>
2022-06-30 15:41:12 +0600

    fix: add space

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,6 +46,7 @@ const Home: NextPage<HomePageProps> = ({ userAgent }) => {
       window.removeEventListener('resize', resizeRefreshDebouncing);
     };
   }, []);
+
   return (
     <MainLayout>
       <Head>
